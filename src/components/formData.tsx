c6e72dd43a40751e6a645466bb9ff1838978af15
minhquang2001 <19522099@gm.uit.edu.vn>
2023-08-21 22:31:28 +0700

    fix use client form Data

--- src/components/formData.tsx
+++ src/components/formData.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState, memo } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
